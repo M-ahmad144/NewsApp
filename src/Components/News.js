4b37adb67e9a18e33c50377b47a310932d94ba96
M-ahmad144 <muhammadahmadmughal0987@gmail.com>
2025-01-19 00:29:59 +0500

    news App

---
+++ src/Components/News.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import "../style.css";
+import Spinner from "./Spinner";
+import NewsItem from "../Components/NewsItem";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  useEffect(() => {
+    document.title = `${
+      props.category.charAt(0).toUpperCase() + props.category.slice(1)
+    } - News`;
+    fetchNews();
+  }, []);
+
+  const fetchNews = async (page = 1) => {
+    props.setProgress(50);
+    setLoading(true);
+
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&page=${page}&pageSize=${props.pageSize}&apiKey=11e8a3b8ec29467ab70d878333d8d56b`;
+
+    try {
+      const response = await fetch(url);
+      props.setProgress(70);
+      const parseData = await response.json();
+
+      setArticles((prevArticles) =>
+        page === 1
+          ? parseData.articles
+          : prevArticles.concat(parseData.articles)
+      );
+      setTotalResults(parseData.totalResults);
+      setPage(page);
+    } catch (error) {
+      console.error("Error fetching news:", error);
+    } finally {
+      setLoading(false);
+      props.setProgress(100);
+    }
+  };
+
+  const fetchMoreData = async () => {
+    if (articles.length < totalResults) {
+      fetchNews(page + 1);
+    }
+  };
+
+  return (
+    <div className="container my-2">
+      <h2 className="my-5 text-center">
+        <strong>
+          Top Headlines -{" "}
+          {props.category.charAt(0).toUpperCase() + props.category.slice(1)}
+        </strong>
+      </h2>
+
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length < totalResults}
+        loader={<Spinner />}
+      >
+        {loading && <Spinner />}
+        {Array.isArray(articles) && (
+          <div className="container">
+            <div className="row">
+              {articles.map((article, index) => (
+                <div className="col-md-4" key={index}>
+                  <NewsItem
+                    title={article.title}
+                    description={article.description}
+                    imageUrl={article.urlToImage}
+                    newsUrl={article.url}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+export default News;
